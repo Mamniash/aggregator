19e01aa15ee84d67fb1e9d57e014a03a46c18a1d
mamniash <amamniashvili@syntella.ru>
2025-03-06 16:06:57 +0300

    problem with yandex api

---
+++ src/pages/MainPage copy.tsx
@@ -0,0 +1,255 @@
+import { useState, useEffect } from 'react'
+import ProductList from '../components/common/ProductList'
+import WhatInTheBox from '../components/common/WhatInTheBox'
+import Order from '../components/common/Order'
+import Footer from '../components/common/Footer'
+import CartIcon from '../components/common/CartIcon'
+import ProductPopup from '../components/common/ProductPopup'
+import CartPage from './CartPage'
+import { productList } from '../data/productList'
+import { ProductType } from '../components/common/Product'
+import { Box, Typography, Modal } from '@mui/material'
+import 'react-toastify/dist/ReactToastify.css'
+import WelcomeScreen from './WelcomeScreen'
+import { styled } from '@mui/system'
+
+export interface FormData {
+	name: string
+	phone: string
+	telegram: string
+	promoCode: string
+	comment: string
+}
+
+export interface CartItem {
+	title: string
+	quantity: number
+	price: string
+	color?: string
+	caseType?: string | null
+}
+
+const ScrollContainer = styled(Box)({
+	'&::-webkit-scrollbar': {
+		width: '8px'
+	},
+	'&::-webkit-scrollbar-thumb': {
+		backgroundColor: '#888',
+		borderRadius: '4px'
+	},
+	'&::-webkit-scrollbar-thumb:hover': {
+		backgroundColor: '#555'
+	},
+	margin: '0 auto',
+	position: 'relative',
+	backgroundColor: 'white',
+	minHeight: '100vh',
+	overflowX: 'hidden'
+})
+
+const MainPage: React.FC = () => {
+	const [formData, setFormData] = useState<FormData>({
+		name: "",
+		phone: "",
+		telegram: "",
+		promoCode: "",
+		comment: ""
+	});
+
+	const [cart, setCart] = useState<CartItem[]>([]);
+	const [isCartOpen, setIsCartOpen] = useState(false);
+	const [selectedProduct, setSelectedProduct] = useState<ProductType | null>(
+		null
+	);
+
+	useEffect(() => {
+		const savedCart = localStorage.getItem("cart");
+		if (savedCart) setCart(JSON.parse(savedCart));
+		setCart([]);
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem("cart", JSON.stringify(cart));
+	}, [cart]);
+
+	const totalAmount = cart
+		.reduce(
+			(acc, item) =>
+				acc + item.quantity * parseFloat(item.price.replace(/\s+/g, "")),
+			0
+		)
+		.toFixed(2);
+
+	const toggleCartItem = (
+		product: ProductType,
+		quantity: number,
+		color: string,
+		caseType: string | null
+	) => {
+		setCart((prevCart) => {
+			const existingItem = prevCart.find(
+				(item) =>
+					item.title === product.title &&
+					item.color === color &&
+					item.caseType === caseType
+			);
+			if (existingItem) {
+				return prevCart.map((item) =>
+					item.title === product.title &&
+					item.color === color &&
+					item.caseType === caseType
+						? { ...item, quantity: item.quantity + quantity }
+						: item
+				);
+			}
+			return [
+				...prevCart,
+				{
+					title: product.title,
+					quantity,
+					price: product.price,
+					color,
+					caseType
+				}
+			];
+		});
+	};
+
+	const totalItems = cart.reduce((acc, item) => acc + item.quantity, 0);
+
+	const scrollToOrder = () => {
+		setIsCartOpen(false);
+		setTimeout(() => {
+			document
+				.getElementById("order-section")
+				?.scrollIntoView({ behavior: "smooth" });
+		}, 300);
+	};
+
+	const clearCart = () => setCart([]);
+
+	const handleProductClick = (product: ProductType) => {
+		setSelectedProduct(product);
+	};
+
+	const scrollToMain = () => {
+		document
+			.getElementById("main-content")
+			?.scrollIntoView({ behavior: "smooth" });
+	};
+
+	// Disable scrolling when a modal is open
+	useEffect(() => {
+		if (isCartOpen || selectedProduct) {
+			// Disable scroll on both body and html
+			document.body.style.overflow = "hidden";
+			document.documentElement.style.overflow = "hidden";
+		} else {
+			// Restore scroll
+			document.body.style.overflow = "auto";
+			document.documentElement.style.overflow = "auto";
+		}
+
+		// Clean up on component unmount or state change
+		return () => {
+			document.body.style.overflow = "auto";
+			document.documentElement.style.overflow = "auto";
+		};
+	}, [isCartOpen, selectedProduct]);
+
+	return (
+		<ScrollContainer>
+			<Box
+				id="welcome-screen"
+				sx={{
+					width: "100%",
+					height: "100vh",
+					backgroundColor: "white",
+					zIndex: 1400
+				}}
+			>
+				<WelcomeScreen onEnter={scrollToMain} />
+			</Box>
+
+			<Box id="main-content">
+				{totalItems > 0 && (
+					<Box
+						sx={{
+							position: "fixed",
+							top: 16,
+							right: 16,
+							zIndex: 1300,
+							transition: "opacity 0.5s",
+							opacity: 1
+						}}
+					>
+						<CartIcon cart={cart} onClick={() => setIsCartOpen(true)} />
+					</Box>
+				)}
+				<Box
+					sx={{
+						pt: { xs: 5, md: 8 }
+					}}
+				>
+					<Typography
+						variant="h4"
+						color="primary"
+						align="center"
+						gutterBottom
+						sx={{ marginBottom: 4 }}
+					>
+						Наши продукты
+					</Typography>
+					<ProductList
+						productList={productList}
+						toggleCartItem={toggleCartItem}
+						cart={cart}
+						onProductClick={handleProductClick}
+					/>
+					<WhatInTheBox />
+					<Box id="order-section">
+						<Order
+							formData={formData}
+							setFormData={setFormData}
+							cartItems={cart}
+							totalAmount={totalAmount}
+							clearCart={clearCart}
+						/>
+					</Box>
+				</Box>
+				<Footer />
+
+				<Modal open={isCartOpen} onClose={() => setIsCartOpen(false)}>
+					<Box
+						sx={{
+							position: "absolute",
+							top: "50%",
+							left: "50%",
+							transform: "translate(-50%, -50%)",
+							width: "90%",
+							maxWidth: 500
+						}}
+					>
+						<CartPage
+							cart={cart}
+							clearCart={clearCart}
+							onOrderClick={scrollToOrder}
+							totalAmount={totalAmount}
+						/>
+					</Box>
+				</Modal>
+
+				{selectedProduct && (
+					<ProductPopup
+						open={Boolean(selectedProduct)}
+						onClose={() => setSelectedProduct(null)}
+						product={selectedProduct}
+						toggleCartItem={toggleCartItem}
+					/>
+				)}
+			</Box>
+		</ScrollContainer>
+	);
+};
+
+export default MainPage
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,255 +1,164 @@
-import { useState, useEffect } from 'react'
-import ProductList from '../components/common/ProductList'
-import WhatInTheBox from '../components/common/WhatInTheBox'
-import Order from '../components/common/Order'
-import Footer from '../components/common/Footer'
-import CartIcon from '../components/common/CartIcon'
-import ProductPopup from '../components/common/ProductPopup'
-import CartPage from './CartPage'
-import { productList } from '../data/productList'
-import { ProductType } from '../components/common/Product'
-import { Box, Typography, Modal } from '@mui/material'
-import 'react-toastify/dist/ReactToastify.css'
-import WelcomeScreen from './WelcomeScreen'
-import { styled } from '@mui/system'
+import React, { useState } from "react";
+import { Button, Card, Col, Row, message, Spin } from "antd";
+import axios from "axios"; // Используем axios для запросов
 
-export interface FormData {
-	name: string
-	phone: string
-	telegram: string
-	promoCode: string
-	comment: string
+interface Product {
+	id: number;
+	name: string;
+	image: string;
 }
 
-export interface CartItem {
-	title: string
-	quantity: number
-	price: string
-	color?: string
-	caseType?: string | null
-}
+const MainPage: React.FC = () => {
+	// Продукты, которые будут отображаться на странице
+	const initialProducts: Product[] = [
+		{
+			id: 1,
+			name: "Куриная грудка",
+			image: "/chicken.jpg"
+		},
+		{ id: 2, name: "Яйца", image: "/eggs.jpg" },
+		{ id: 3, name: "Горох", image: "/peas.jpg" }
+		// { id: 4, name: "Хлеб", image: "/chicken.jpg" }
+		// { id: 5, name: "Бананы", image: "/peas.jpg" }
+	];
 
-const ScrollContainer = styled(Box)({
-	'&::-webkit-scrollbar': {
-		width: '8px'
-	},
-	'&::-webkit-scrollbar-thumb': {
-		backgroundColor: '#888',
-		borderRadius: '4px'
-	},
-	'&::-webkit-scrollbar-thumb:hover': {
-		backgroundColor: '#555'
-	},
-	margin: '0 auto',
-	position: 'relative',
-	backgroundColor: 'white',
-	minHeight: '100vh',
-	overflowX: 'hidden'
-})
+	const [products, setProducts] = useState<Product[]>(initialProducts);
+	const [cart, setCart] = useState<Product[]>([]);
+	const [loading, setLoading] = useState(false);
+	const [prices, setPrices] = useState<Map<number, number>>(new Map());
 
-function MainPage() {
-	const [formData, setFormData] = useState<FormData>({
-		name: '',
-		phone: '',
-		telegram: '',
-		promoCode: '',
-		comment: ''
-	})
+	// API ключ Яндекс Еды
+	const API_KEY = "AQVN1E_-bD6P1EHUsBfEWqo7Ud4sHDp3cEpEJBVt"; // Замените на свой API ключ
+	const API_URL =
+		"https://cors-anywhere.herokuapp.com/https://eda.yandex/api/v2/products/get_prices";
 
-	const [cart, setCart] = useState<CartItem[]>([])
-	const [isCartOpen, setIsCartOpen] = useState(false)
-	const [selectedProduct, setSelectedProduct] = useState<ProductType | null>(
-		null
-	)
+	// Добавление продукта в корзину
+	const addToCart = (product: Product) => {
+		setCart((prev) => [...prev, product]);
+	};
 
-	useEffect(() => {
-		const savedCart = localStorage.getItem('cart')
-		if (savedCart) setCart(JSON.parse(savedCart))
-		setCart([])
-	}, [])
+	// Получение цен из API Яндекс Еды
+	const fetchPrices = async () => {
+		if (cart.length === 0) {
+			message.warning("Сначала добавьте продукты в корзину!");
+			return;
+		}
 
-	useEffect(() => {
-		localStorage.setItem('cart', JSON.stringify(cart))
-	}, [cart])
-
-	const totalAmount = cart
-		.reduce(
-			(acc, item) =>
-				acc + item.quantity * parseFloat(item.price.replace(/\s+/g, '')),
-			0
-		)
-		.toFixed(2)
-
-	const toggleCartItem = (
-		product: ProductType,
-		quantity: number,
-		color: string,
-		caseType: string | null
-	) => {
-		setCart((prevCart) => {
-			const existingItem = prevCart.find(
-				(item) =>
-					item.title === product.title &&
-					item.color === color &&
-					item.caseType === caseType
-			)
-			if (existingItem) {
-				return prevCart.map((item) =>
-					item.title === product.title &&
-					item.color === color &&
-					item.caseType === caseType
-						? { ...item, quantity: item.quantity + quantity }
-						: item
-				)
-			}
-			return [
-				...prevCart,
+		setLoading(true);
+		try {
+			// Подготовка списка товаров для запроса
+			const productNames = cart.map((item) => item.name);
+			const response = await axios.post(
+				API_URL,
 				{
-					title: product.title,
-					quantity,
-					price: product.price,
-					color,
-					caseType
+					product_names: productNames
+				},
+				{
+					headers: {
+						Authorization: `Bearer ${API_KEY}`,
+						"Content-Type": "application/json"
+					}
 				}
-			]
-		})
-	}
+			);
 
-	const totalItems = cart.reduce((acc, item) => acc + item.quantity, 0)
+			// Пример обработки полученных данных
+			const pricesFromAPI = response.data.data;
+			const updatedPrices = new Map<number, number>();
 
-	const scrollToOrder = () => {
-		setIsCartOpen(false)
-		setTimeout(() => {
-			document
-				.getElementById('order-section')
-				?.scrollIntoView({ behavior: 'smooth' })
-		}, 300)
-	}
+			pricesFromAPI.forEach((priceData: { name: string; price: number }) => {
+				const product = cart.find((p) => p.name === priceData.name);
+				if (product) {
+					updatedPrices.set(product.id, priceData.price);
+				}
+			});
 
-	const clearCart = () => setCart([])
-
-	const handleProductClick = (product: ProductType) => {
-		setSelectedProduct(product)
-	}
-
-	const scrollToMain = () => {
-		document
-			.getElementById('main-content')
-			?.scrollIntoView({ behavior: 'smooth' })
-	}
-
-	// Disable scrolling when a modal is open
-	useEffect(() => {
-		if (isCartOpen || selectedProduct) {
-			// Disable scroll on both body and html
-			document.body.style.overflow = 'hidden'
-			document.documentElement.style.overflow = 'hidden'
-		} else {
-			// Restore scroll
-			document.body.style.overflow = 'auto'
-			document.documentElement.style.overflow = 'auto'
+			setPrices(updatedPrices);
+		} catch (error) {
+			console.error("Ошибка при получении цен:", error);
+			message.error("Ошибка при получении цен. Попробуйте снова.");
+		} finally {
+			setLoading(false);
 		}
-
-		// Clean up on component unmount or state change
-		return () => {
-			document.body.style.overflow = 'auto'
-			document.documentElement.style.overflow = 'auto'
-		}
-	}, [isCartOpen, selectedProduct])
+	};
 
 	return (
-		<ScrollContainer>
-			<Box
-				id='welcome-screen'
-				sx={{
-					width: '100%',
-					height: '100vh',
-					backgroundColor: 'white',
-					zIndex: 1400
-				}}
-			>
-				<WelcomeScreen onEnter={scrollToMain} />
-			</Box>
+		<div className="p-6">
+			<h1 className="text-xl font-bold text-center mb-6">
+				Добро пожаловать в наш Агрегатор!
+			</h1>
 
-			<Box id='main-content'>
-				{totalItems > 0 && (
-					<Box
-						sx={{
-							position: 'fixed',
-							top: 16,
-							right: 16,
-							zIndex: 1300,
-							transition: 'opacity 0.5s',
-							opacity: 1
-						}}
-					>
-						<CartIcon cart={cart} onClick={() => setIsCartOpen(true)} />
-					</Box>
+			{/* Список продуктов */}
+			<Row gutter={16}>
+				{products.map((product) => (
+					<Col span={8} key={product.id} className="mb-4">
+						<Card
+							hoverable
+							cover={
+								<img
+									alt={product.name}
+									src={product.image}
+									className="w-[360px] h-[360px]"
+								/>
+							}
+							actions={[
+								<Button
+									type="primary"
+									onClick={() => addToCart(product)}
+								>
+									Добавить в корзину
+								</Button>
+							]}
+						>
+							<Card.Meta title={product.name} />
+						</Card>
+					</Col>
+				))}
+			</Row>
+
+			{/* Корзина */}
+			<div className="mt-6">
+				<h2 className="text-lg font-semibold">Ваша корзина</h2>
+				{cart.length > 0 ? (
+					<ul>
+						{cart.map((item) => (
+							<li key={item.id}>{item.name}</li>
+						))}
+					</ul>
+				) : (
+					<p>Корзина пуста</p>
 				)}
-				<Box
-					sx={{
-						pt: { xs: 5, md: 8 }
-					}}
+			</div>
+
+			{/* Кнопка для получения цен */}
+			<div className="mt-6 text-center">
+				<Button
+					type="primary"
+					size="large"
+					onClick={fetchPrices}
+					disabled={loading}
 				>
-					<Typography
-						variant='h4'
-						color='primary'
-						align='center'
-						gutterBottom
-						sx={{ marginBottom: 4 }}
-					>
-						Наши продукты
-					</Typography>
-					<ProductList
-						productList={productList}
-						toggleCartItem={toggleCartItem}
-						cart={cart}
-						onProductClick={handleProductClick}
-					/>
-					<WhatInTheBox />
-					<Box id='order-section'>
-						<Order
-							formData={formData}
-							setFormData={setFormData}
-							cartItems={cart}
-							totalAmount={totalAmount}
-							clearCart={clearCart}
-						/>
-					</Box>
-				</Box>
-				<Footer />
+					{loading ? <Spin /> : "Получить цены"}
+				</Button>
+			</div>
 
-				<Modal open={isCartOpen} onClose={() => setIsCartOpen(false)}>
-					<Box
-						sx={{
-							position: 'absolute',
-							top: '50%',
-							left: '50%',
-							transform: 'translate(-50%, -50%)',
-							width: '90%',
-							maxWidth: 500
-						}}
-					>
-						<CartPage
-							cart={cart}
-							clearCart={clearCart}
-							onOrderClick={scrollToOrder}
-							totalAmount={totalAmount}
-						/>
-					</Box>
-				</Modal>
+			{/* Показ цен */}
+			{prices.size > 0 && (
+				<div className="mt-6">
+					<h2 className="text-lg font-semibold">Цены на продукты</h2>
+					<ul>
+						{cart.map((item) => (
+							<li key={item.id}>
+								{item.name}:{" "}
+								{prices.get(item.id)
+									? `${prices.get(item.id)} ₽`
+									: "Цена не найдена"}
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
+		</div>
+	);
+};
 
-				{selectedProduct && (
-					<ProductPopup
-						open={Boolean(selectedProduct)}
-						onClose={() => setSelectedProduct(null)}
-						product={selectedProduct}
-						toggleCartItem={toggleCartItem}
-					/>
-				)}
-			</Box>
-		</ScrollContainer>
-	)
-}
-
-export default MainPage
+export default MainPage;
